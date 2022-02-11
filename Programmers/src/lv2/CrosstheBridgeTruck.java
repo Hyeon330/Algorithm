b5aedb052608ada818c03726d138bd843aafff3a
이현수 <dldlsrn231@gmail.com>
2022-02-11 22:43:08 +0900

    update

---
+++ Programmers/src/lv2/CrosstheBridgeTruck.java
@@ -0,0 +1,17 @@
+package lv2;
+
+public class CrosstheBridgeTruck {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CrosstheBridgeTruck cbt = new CrosstheBridgeTruck();
+        int bridge_length = 100;
+        int weight = 100;
+        int[] truck_weights = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+
+        System.out.println(cbt.solution(bridge_length, weight, truck_weights));
+    }
+}
--- Programmers/src/lv2/Printer.java
+++ Programmers/src/lv2/Printer.java
@@ -12,29 +12,74 @@ public class Printer {
             qu.offer(integer);
         }
         boolean ck = true;
+        // while (ck) {
+        // // int imp = qu.poll();
+        // for (int i = 0; i < qu.size(); i++) {
+        // if (/* img */qu.peek() < li.get(i)) {
+        // qu.offer(/* imp */qu.poll());
+        // break;
+        // }
+        // if (i == qu.size() - 1) {
+        // qu.remove(); // <- 없었음.
+        // answer++;
+        // if (location == 0) {
+        // ck = false;
+        // }
+        // }
+        // }
+        // System.out.println(qu);
+        // if (location == 0) {
+        // location = qu.size() - 1;
+        // } else {
+        // location--;
+        // }
+        // }
+
+        sort(priorities, 0, priorities.length - 1);
+        int size = priorities.length - 1;
+
         while (ck) {
-            // int imp = qu.poll();
-            for (int i = 0; i < qu.size(); i++) {
-                if (/* img */qu.peek() < li.get(i)) {
-                    qu.offer(/* imp */qu.poll());
-                    break;
-                }
-                if (i == qu.size() - 1) {
-                    qu.remove(); // <- 없었음.
-                    answer++;
-                    if (location == 0) {
-                        ck = false;
-                    }
-                }
-            }
-            System.out.println(qu);
-            if (location == 0) {
-                location = qu.size() - 1;
-            } else {
+            if (qu.peek() == priorities[size - answer]) {
+                qu.remove();
                 location--;
+                answer++;
+                if (location < 0)
+                    ck = false;
+            } else {
+                qu.add(qu.poll());
+                location--;
+                if (location < 0)
+                    location = qu.size() - 1;
             }
         }
+
         return answer;
+    }
+
+    public void sort(int[] arr, int left, int right) {
+        int pl = left;
+        int pr = right;
+        int pi = arr[(pl + pr) / 2];
+
+        do {
+            while (arr[pl] < pi)
+                pl++;
+            while (arr[pr] > pi)
+                pr--;
+
+            if (pl <= pr) {
+                int tmp = arr[pl];
+                arr[pl] = arr[pr];
+                arr[pr] = tmp;
+                pl++;
+                pr--;
+            }
+        } while (pl <= pr);
+
+        if (pl < right)
+            sort(arr, pl, right);
+        if (left < pr)
+            sort(arr, left, pr);
     }
 
     public static void main(String[] args) {
