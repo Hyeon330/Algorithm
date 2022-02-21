747b81f91f4d2dabdb2fa64b9ba5508bef56b6c4
이현수 <dldlsrn231@gmail.com>
2022-02-21 13:05:10 +0900

    update

--- Programmers/src/lv3/BestAlbum.java
+++ Programmers/src/lv3/BestAlbum.java
@@ -1,9 +1,98 @@
 package lv3;
 
+import java.util.*;
+import java.util.Map.Entry;
+
 // 해시 > 베스트앨범
 
 public class BestAlbum {
     public int[] solution(String[] genres, int[] plays) {
+        // HashMap<String, Integer> mapSum = new HashMap<>();
+        // for (int i = 0; i < genres.length; i++) {
+        // mapSum.put(genres[i], mapSum.getOrDefault(genres[i], 0) + plays[i]);
+        // }
+        // HashMap<String, ArrayList<Integer>> mapNumArr = new HashMap<>();
+        // for (int i = 0; i < genres.length; i++) {
+        // if (!mapNumArr.containsKey(genres[i])) {
+        // mapNumArr.put(genres[i], new ArrayList<>());
+        // }
+        // mapNumArr.get(genres[i]).add(plays[i]);
+        // }
+        // List<Entry<String, Integer>> listEntryes = new ArrayList<Entry<String,
+        // Integer>>(mapSum.entrySet());
+        // Collections.sort(listEntryes, new Comparator<Entry<String, Integer>>() {
+        // @Override
+        // public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+        // return o2.getValue().compareTo(o1.getValue());
+        // }
+        // });
+        // ArrayList<Integer> arr = new ArrayList<>();
+        // for (Entry<String, Integer> entry : listEntryes) {
+        // Collections.sort(mapNumArr.get(entry.getKey()), Collections.reverseOrder());
+        // for (int i = 0; i < 2; i++) {
+        // for (int j = 0; j < plays.length; j++) {
+        // if (mapNumArr.get(entry.getKey()).get(i) == plays[j]) {
+        // arr.add(j);
+        // break;
+        // }
+        // }
+        // }
+        // }
+        // int[] answer = new int[arr.size()];
+        // for (int i = 0; i < arr.size(); i++) {
+        // answer[i] = arr.get(i);
+        // }
+        // return answer;
+
+        // -------------------------------------------------------------
+
+        // HashMap<String, Integer> mapSum = new HashMap<>();
+        // HashMap<String, HashMap<Integer, ArrayList<Integer>>> map = new HashMap<>();
+        // for (int i = 0; i < genres.length; i++) {
+        // mapSum.put(genres[i], mapSum.getOrDefault(genres[i], 0) + plays[i]);
+        // }
+        // System.out.println(mapSum);
+        // for (int i = 0; i < genres.length; i++) {
+        // if (!map.containsKey(genres[i])) {
+        // HashMap<Integer, ArrayList<Integer>> mapArr = new HashMap<>();
+        // mapArr.put(mapSum.get(genres[i]), new ArrayList<Integer>());
+        // map.put(genres[i], mapArr);
+        // }
+        // map.get(genres[i]).get(mapSum.get(genres[i])).add(plays[i]);
+        // }
+        // List<Entry<String, HashMap<Integer, ArrayList<Integer>>>> listEntryes = new
+        // ArrayList<>(map.entrySet());
+        // Collections.sort(listEntryes, new Comparator<Entry<String, HashMap<Integer,
+        // ArrayList<Integer>>>>() {
+        // @Override
+        // public int compare(Entry<String, HashMap<Integer, ArrayList<Integer>>> o1,
+        // Entry<String, HashMap<Integer, ArrayList<Integer>>> o2) {
+        // return o2.getKey().compareTo(o1.getKey());
+        // }
+        // });
+        // ArrayList<Integer> answerArr = new ArrayList<>();
+        // for (int i = 0; i < listEntryes.size(); i++) {
+        // Collections.sort(listEntryes.get(i).getValue().get(mapSum.get(listEntryes.get(i).getKey())),
+        // Collections.reverseOrder());
+        // for (int j = 0; j < 2; j++) {
+        // for (int j2 = 0; j2 < plays.length; j2++) {
+        // if (plays[j2] ==
+        // listEntryes.get(i).getValue().get(mapSum.get(listEntryes.get(i).getKey()))
+        // .get(j)) {
+        // answerArr.add(j2);
+        // break;
+        // }
+        // }
+        // }
+        // }
+        // int[] answer = new int[answerArr.size()];
+        // for (int i = 0; i < answerArr.size(); i++) {
+        // answer[i] = answerArr.get(i);
+        // }
+        // return answer;
+
+        // ---------------------------------------------------------------------------------------
+
         int[] answer = {};
         return answer;
     }
@@ -13,6 +102,6 @@ public class BestAlbum {
         String[] genres = { "classic", "pop", "classic", "classic", "pop" };
         int[] plays = { 500, 600, 150, 800, 2500 };
 
-        ba.solution(genres, plays);
+        System.out.println(Arrays.toString(ba.solution(genres, plays)));
     }
 }
--- Programmers/src/test.java
+++ Programmers/src/test.java
@@ -1,22 +1,38 @@
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class test {
     public static void main(String[] args) {
-        String[] phone_book = { "119", "97674223", "1195524421" };
-        // String[] phone_book = { "123", "456", "789" };
-        // String[] phone_book = { "12", "123", "1235", "567", "88" };
+        Map<String, Integer> testMap = new HashMap<String, Integer>();
 
-        HashSet<String> set = new HashSet<>();
-        for (int i = 0; i < phone_book.length; i++) {
-            set.add(phone_book[i]);
-        }
+        // Map에 데이터 추가
+        testMap.put("apple", 1);
+        testMap.put("orange", 2);
+        testMap.put("pineapple", 4);
+        testMap.put("strawberry", 5);
+        testMap.put("melon", 3);
 
-        for (int i = 0; i < phone_book.length; i++) {
-            for (int j = 0; j < phone_book[i].length(); j++) {
-                if (set.contains(phone_book[i].substring(0, j))) {
-                    System.out.println(phone_book[i]);
-                }
+        // Map.Entry 리스트 작성
+        List<Entry<String, Integer>> list_entries = new ArrayList<Entry<String, Integer>>(testMap.entrySet());
+
+        // 비교함수 Comparator를 사용하여 오름차순으로 정렬
+        Collections.sort(list_entries, new Comparator<Entry<String, Integer>>() {
+            // compare로 값을 비교
+            public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2) {
+                // 오름 차순 정렬
+                return obj2.getValue().compareTo(obj1.getValue());
             }
+        });
+
+        System.out.println("오름 차순 정렬");
+        // 결과 출력
+        for (Entry<String, Integer> entry : list_entries) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 }
