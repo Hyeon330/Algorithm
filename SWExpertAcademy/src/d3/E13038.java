d527b1ed2bd7e6e88d0bdabe1b463fc1c1256964
hyeon3 <dldlsrn231@gmail.com>
2022-05-25 18:16:45 +0900

    update

---
+++ SWExpertAcademy/src/d3/E13038.java
@@ -0,0 +1,7 @@
+package d3;
+
+public class E13038 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ SWExpertAcademy/src/d3/E13229.java
@@ -0,0 +1,28 @@
+package d3;
+
+import java.io.*;
+import java.util.*;
+
+public class E13229 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		List<String> week = new ArrayList<String>(Arrays.asList("MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"));
+		int weekL = week.size();
+		
+		int T = Integer.parseInt(bf.readLine());
+		
+		StringBuffer sb = new StringBuffer();
+		for(int i=1; i<=T; i++) {
+			sb.append("#"+i+" ");
+			String S = bf.readLine();
+			int day = 7;
+			int cnt = 0;
+			for(int j=week.indexOf(S)+1; j<weekL; j++) {
+				day=++cnt;
+			}
+			sb.append(day+"\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
