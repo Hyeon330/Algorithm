291eaf5959b96723e37ad46e7e9534df8b520017
hyeon3 <dldlsrn231@gmail.com>
2022-06-05 22:25:22 +0900

    update

---
+++ Theory/src/dynamicProg/Fibonacci.java
@@ -0,0 +1,18 @@
+package dynamicProg;
+
+public class Fibonacci {
+	
+	static int F(int n) {
+		if(n==0) {
+			return 0;
+		}else if (n==1) {
+			return 1;
+		}else {
+			return F(n-1) + F(n-2);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(F(35));
+	}
+}
