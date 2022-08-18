c7a71c3cb44c09f904515aa191a2808d667a0081
hyeon3 <dldlsrn231@gmail.com>
2022-08-18 23:14:49 +0900

    update

--- BaekJoon/src/DynamicProg/Ex2839.java
+++ BaekJoon/src/DynamicProg/Ex2839.java
@@ -8,27 +8,43 @@ public class Ex2839 {
 		int N = Integer.parseInt(bf.readLine());
 		
 		if(N < 3) System.out.println(-1);
-		else {
-			System.out.println(count(5 * (N / 5)));
-		}
-		
-//		if(N < 3) System.out.println(-1);
-//		else if(N % 5 == 0) System.out.println(N/5);
-//		else if(N < 10 && N % 3 == 0) System.out.println(N/3);
-//		else System.out.println(count(N, 5 * (N / 5)));
-		
-		
+		else if(N % 5 == 0) System.out.println(N/5);
+		else if(N < 10 && N % 3 == 0) System.out.println(N/3);
+		else System.out.println(count(N, 5 * (N / 5)));
 	}
 	
-	static int count(int N) {
-		return 0;
+	public static int count(int N, int n) {
+		if(n < 3) return -1;
+		
+		if((N - n) % 3 == 0) return n / 5 + (N - n) / 3;
+		
+		return count(N, n-5);
 	}
-	
-//	public static int count(int N, int n) {
-//		if(n < 3) return -1;
-//		
-//		if((N - n) % 3 == 0) return n / 5 + (N - n) / 3;
-//		
-//		return count(N, n-5);
-//	}
 }
+
+//import java.io.InputStreamReader;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+// 
+//public class Main {
+// 
+//	public static void main(String[] args) throws IOException {
+//		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int N = Integer.parseInt(br.readLine());
+//		
+// 
+//		if (N == 4 || N == 7) {
+//			System.out.println(-1);
+//		}
+//		else if (N % 5 == 0) {
+//			System.out.println(N / 5);
+//		}
+//		else if (N % 5 == 1 || N % 5 == 3) {
+//			System.out.println((N / 5) + 1);
+//		}
+//		else if (N % 5 == 2 || N % 5 == 4) {
+//			System.out.println((N / 5) + 2);
+//		}
+//	}
+//}
