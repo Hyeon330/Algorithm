cf0ce7052a132286856e2d18eb2431ec841d5fbe
이현수 <dldlsrn231@gmail.com>
2022-09-05 23:26:52 +0900

    update

---
+++ BaekJoon/src/dynamicProg/Ex2193.java
@@ -0,0 +1,24 @@
+package dynamicProg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex2193 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(bf.readLine());
+		int dp[] = new int[n];
+		
+		/*
+		 * 1 1
+		 * 2 10
+		 * 3 100 101
+		 * 4 1000 1001 1010
+		 * 5 10000 10001 10010 10100 10101
+		 * 6 100000 100001 100010 100100 100101 101000 101001 101010
+		 *   
+		 */
+	}
+}
