22d2a8b00b27f8ce579d5765675e48e196c81af3
hyeon3 <dldlsrn231@gmail.com>
2022-09-23 00:45:28 +0900

    Ex14501

--- BaekJoon/src/dynamicProg/Ex14501.java
+++ BaekJoon/src/dynamicProg/Ex14501.java
@@ -3,6 +3,7 @@ package dynamicProg;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Ex14501 {
@@ -13,20 +14,24 @@ public class Ex14501 {
 		int[][] arr = new int[n][2];
 		int[] dp = new int[n];
 		
-		while(n-- != 0) {
+		for(int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(bf.readLine());
-			arr[n][0] = Integer.parseInt(st.nextToken());
-			arr[n][1] = Integer.parseInt(st.nextToken());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		dp[0] = arr[0][1];
-		for(int i = 1; i < n; i++) {
-			dp[i] = arr[i][1];
-			for(int j = i - 1; j >= 0; i--) {
-				if(i - j == arr[j][0]) {
-					dp[i] = Math.max(dp[i], arr[i][1] + dp[j]);
+		for(int i = 0; i < n; i++) {
+			if(i + arr[i][0] <= n) {
+				dp[i] = arr[i][1];
+				for(int j = i - 1; j >= 0; j--) {
+					if(i - j >= arr[j][0]) {
+						dp[i] = Math.max(dp[i], arr[i][1] + dp[j]);
+					}
 				}
 			}
 		}
+		
+		Arrays.sort(dp);
+		System.out.println(dp[dp.length - 1]);
 	}
 }
