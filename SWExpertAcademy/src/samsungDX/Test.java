31e91f4bc8c895949e537a311d10e602abdd6046
hyeon3 <dldlsrn231@gmail.com>
2023-01-23 03:08:50 +0900

    update

--- SWExpertAcademy/src/samsungDX/Test.java
+++ SWExpertAcademy/src/samsungDX/Test.java
@@ -1,20 +1,12 @@
 package samsungDX;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.InputStreamReader;
 
 public class Test {
-    static void fun(Integer a) {
-        if (a == 3)
-            return;
-        fun(++a);
-    }
     public static void main(String[] args) throws IOException {
-        LinkedList<Integer> lst1 = new LinkedList<>();
-
+        
     }
 }
