75ca4fccd494fabbf022f01bc7056cad6b343915
hyeon3 <dldlsrn231@gmail.com>
2023-02-09 21:43:27 +0900

    update

--- SWExpertAcademy/src/samsungDX/Test.java
+++ SWExpertAcademy/src/samsungDX/Test.java
@@ -1,88 +1,22 @@
 package samsungDX;
 
-<<<<<<< HEAD
-class Node {
-    int data;
-    Node next;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    Node(int data) {
-        this.data = data;
-        next = null;
-    }
-}
-
-class SinglyList {
-    static final int MAX_NODE = 1000;
-
-    Node[] nodePool = new Node[MAX_NODE];
-    Node head;
-    int nodeCount = 0;
-
-    SinglyList() {
-        head = new Node(0);
-        init();
-    }
-
-    private Node newNode(int data) {
-        nodePool[nodeCount] = new Node(data);
-        return nodePool[nodeCount++];
-    }
-
-    void init() {
-        head.next = null;
-        nodeCount = 0;
-    }
-
-    void insert(int x) {
-        Node node = newNode(x);
-
-        node.next = head.next;
-        head.next = node;
-    }
-
-    void remove(int x) {
-        Node prev = head;
-        while (prev.next != null && prev.next.data != x) prev = prev.next;
-        if (prev.next != null) prev.next = prev.next.next;
-    }
-
-    boolean contains(int x) {
-        Node n = head.next;
-        while (n != null && n.data != x) n = n.next;
-        return n != null;
-    }
-
-    @Override
-    public String toString() {
+public class Test {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
-        Node n = head.next;
+        StringTokenizer st;
+        int T;
 
-        sb.append("[List]");
-        while (n != null) {
-            sb.append(n.data);
-            if (n.next != null) sb.append(" -> ");
-            n = n.next;
+        T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+
+            sb.append("#" + tc + " " + "\n");
         }
-        return sb.toString();
-    }
-}
-
-public class Test {
-    public static void main(String[] args) {
-
-=======
-import java.util.PriorityQueue;
-import java.util.Set;
-
-public class Test {
-    public static void main(String[] args) {
-        char[] name = "aa ".toCharArray();
-        name[2] = 0;
-        int a = 0;
-        for (int i = 0; name[i] != 0; i++) {
-            a = a << 5 | (name[i] ^ 96);
-        }
-        System.out.println(a);
->>>>>>> 3362433cf1c3b7aade13f7883d1ae078545e4e62
+        System.out.print(sb);
     }
 }
